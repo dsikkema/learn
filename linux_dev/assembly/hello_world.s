49ef203c65817869d3cbb923e99d82c88793f812
dale <dalesikkema@gmail.com>
2025-03-01 16:03:52 -0600

    assembly: hello world (write syscall, ascii str)

---
+++ linux_dev/assembly/hello_world.s
@@ -0,0 +1,14 @@
+.global _start
+_start:
+    mov x8, #64             // 64: write syscall
+    mov x0, #1              // file descriptor (1: stdout)
+    adr x1, hello_str       // data address
+    mov x2, #13             // buffer len, len(hello_str) == 13
+    svc #0
+    
+    mov x0, #0              // Start exiting
+    mov x8, #93
+    svc #0
+
+hello_str:
+    .ascii "Hello, world\n"
